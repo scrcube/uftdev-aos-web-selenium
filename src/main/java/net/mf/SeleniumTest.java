315b6e8dbd7e78b4e635f1f53608af90c1f9b2dd
Jeff Clare <jeff.clare@hpe.com>
2017-12-05 13:22:06 -0700

    Completion of Selenium script

--- src/main/java/net/mf/SeleniumTest.java
+++ src/main/java/net/mf/SeleniumTest.java
@@ -1,27 +1,24 @@
 package net.mf;
 
-import static org.junit.Assert.*;
-
-import com.hpe.leanft.selenium.Utils;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.pagefactory.ByChained;
 import org.openqa.selenium.*;
 import com.hpe.leanft.selenium.By;
-import com.hpe.leanft.selenium.Utils.*;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import com.hpe.leanft.selenium.Utils;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
-import java.util.HashMap;
-import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
 public class SeleniumTest  {
-    
+    static final String ADV_WEBSITE  = "insert website here";
+    static final String ADV_LOGIN    = "insert login name here";
+    static final String ADV_PASSWORD = "insert password here";
+
     private static WebDriver driver;
     public SeleniumTest() {
         //Change this constructor to private if you supply your own public constructor
@@ -42,7 +39,6 @@ public class SeleniumTest  {
 
     @Before
     public void setUp() throws Exception {
-
     }
 
     @After
@@ -51,21 +47,65 @@ public class SeleniumTest  {
 
     @Test
     public void corndog() throws Exception {
-        driver.get("http://www.advantageonlineshopping.com");
+        driver.get(ADV_WEBSITE);
 
         driver.manage().window().maximize();
         WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        //Login to Advantage
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/header/nav/ul/li[2]/a")));
+        Utils.highlight(driver.findElement(By.xpath("/html/body/header/nav/ul/li[2]/a")), 1000);
+        driver.findElement(By.xpath("/html/body/header/nav/ul/li[2]/a")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[1]/div/input")));
+        Utils.highlight(driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[1]/div/input")), 1000);
+        driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[1]/div/input")).sendKeys(ADV_LOGIN);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[2]/div/input")));
+        Utils.highlight(driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[2]/div/input")), 1000);
+        driver.findElement(By.xpath("/html/body/login-modal/div/div/div[3]/sec-form/sec-view[2]/div/input")).sendKeys(ADV_PASSWORD);
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("SIGN IN")));
+        Utils.highlight(driver.findElement(By.visibleText("SIGN IN")), 1000);
+        driver.findElement(By.visibleText("SIGN IN")).click();
 
         //Click on Tablets
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("TABLETS")));
         Utils.highlight(driver.findElement(By.visibleText("TABLETS")), 1000);
         driver.findElement(By.visibleText("TABLETS")).click();
 
-
         //Click on specific tablet
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("HP Pro Tablet 608 G1")));
         Utils.highlight(driver.findElement(By.visibleText("HP Pro Tablet 608 G1")), 1000);
         driver.findElement(By.visibleText("HP Pro Tablet 608 G1")).click();
+
+        //Add Tablet to cart
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("ADD TO CART")));
+        Utils.highlight(driver.findElement(By.visibleText(("ADD TO CART"))), 1000);
+        driver.findElement(By.visibleText("ADD TO CART")).click();
+
+        //Go to Checkout
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText(Pattern.compile("CHECKOUT \\(\\$*"))));
+        Utils.highlight(driver.findElement(By.visibleText(Pattern.compile("CHECKOUT \\(\\$*"))), 1000);
+        driver.findElement(By.visibleText(Pattern.compile("CHECKOUT \\(\\$*"))).click();
+
+        //Checkout
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("NEXT")));
+        Utils.highlight(driver.findElement(By.visibleText("NEXT")), 1000);
+        driver.findElement(By.visibleText("NEXT")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("PAY NOW")));
+        Utils.highlight(driver.findElement(By.visibleText("PAY NOW")), 1000);
+        driver.findElement(By.visibleText("PAY NOW")).click();
+
+        //Logout of Advantage
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/header/nav/ul/li[2]/a")));
+        Utils.highlight(driver.findElement(By.xpath("/html/body/header/nav/ul/li[2]/a")), 1000);
+        driver.findElement(By.xpath("/html/body/header/nav/ul/li[2]/a")).click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.visibleText("Sign out")));
+        Utils.highlight(driver.findElement(By.visibleText("Sign out")), 1000);
+        driver.findElement(By.visibleText("Sign out")).click();
 
         //Added sleep here to give time to see the selection
         Thread.sleep(3000);
